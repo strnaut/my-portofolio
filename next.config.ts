fe0ec464f3e49193cc5ae5d11f601ab1d3df683f
Farghali Syafyurrahman <Farghali.Rahman@gmail.com>
2026-05-13 09:01:12 +0700

    config: add basePath and assetPrefix for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // Tambahkan dua baris ini:
+  basePath: '/my-portofolio', 
+  assetPrefix: '/my-porotfolio',
 };
 
 export default nextConfig;
